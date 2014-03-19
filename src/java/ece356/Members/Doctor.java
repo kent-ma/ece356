edfc1c3ec9143d8c553bfc608c33e4895ac39693
Freeman Kam <fchkam@gmail.com>
2014-03-19 13:11:06 -0700

    Classes Completed
    Doctor, Patient, Staff classes finished

--- src/java/ece356/Members/Doctor.java
+++ src/java/ece356/Members/Doctor.java
@@ -11,5 +11,24 @@ package ece356.Members;
  * @author Hades
  */
 public class Doctor {
+    private int staffId;
+    private String name;
+    private String department;
+    
+    public Doctor(int staffId, String name, String department){
+        this.staffId = staffId;
+        this.name = name;
+        this.department = department;
+    }
+    
+    public int getStaffId(){return staffId;}
+    public String getName(){return name;}
+    public String getDepartment(){return department;}
+    
+    public void setStaffId(int staffId){this.staffId = staffId;}
+    public void setName(String name){this.name = name;}
+    public void setDepartment(String department){this.department = department;}
+    
+    
     
 }
--- src/java/ece356/Members/Staff.java
+++ src/java/ece356/Members/Staff.java
@@ -11,5 +11,15 @@ package ece356.Members;
  * @author Hades
  */
 public class Staff {
+    private int staffId;
+    private String name;
+    private String department;
     
+    public int getStaffId(){return staffId;}
+    public String getName(){return name;}
+    public String getDepartment(){return department;}
+    
+    public void setStaffId(){this.staffId = staffId;}
+    public void setName(){this.name = name;}
+    public void setDepartment(){this.department = department;}
 }
