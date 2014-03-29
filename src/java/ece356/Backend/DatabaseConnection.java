6e1989ff106a662538d0ce7e64409cd3f3111242
Freeman Kam <fchkam@gmail.com>
2014-03-29 16:59:26 -0700

    SQL protection against DROPs

--- src/java/ece356/Backend/DatabaseConnection.java
+++ src/java/ece356/Backend/DatabaseConnection.java
@@ -201,6 +201,7 @@ public class DatabaseConnection
     public boolean insertRows(String t, String cl, String v) throws SQLException
     {
         String sql = "INSERT INTO " + t + "(" + cl + ") VALUES(" + v + ");";
+        sql.replaceAll("(?i)DROP", "");
         return stmt.execute(sql);
     }
     
